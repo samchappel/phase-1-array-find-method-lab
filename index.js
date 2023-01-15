de126c266f628a915516e76ef6afe82904587b0c
Sam Chappel <m.samchappel@gmail.com>
2023-01-14 17:50:43 -0800

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+// const record = [
+//   { year: "2015", result: "W"},
+//   { year: "2014", result: "N/A"},
+//   { year: "2013", result: "L"},
+//   //...
+// ]
+
+function superbowlWin(record) {
+  const win = record.find(record => record.result === "W")
+  if (win) {
+    return win.year
+  } else {
+    return undefined
+    }
+  }
